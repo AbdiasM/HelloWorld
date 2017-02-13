afe4e42b2055511351cb2608df848dfd3cace27b
Abdias.Michael <abdias.m@gmail.com>
2017-02-13 20:29:39 +0530

    Add

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+//console.log('hello world');
+var express = require('Express');
+var routes = require('./routes');
+var path = require('path');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json({ message: 'Hello! Welcome to Express' });
+});
+
+//app.get('/index', routes.index);
+
+app.get('/index', function (req, res) {
+    res.type('html');
+    res.sendFile("/views/index.html");
+});
+
+app.listen(7001);
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+﻿
+
+exports.index = function (req, res) {
+    res.type = "text/html";
+    res.sendFile('D:\workspace\github\HelloWorld\views\index.html');
+};
