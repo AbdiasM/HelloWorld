b1c11b8a13f759ec8db4b255e8b40bd926f4a0cf
abdias.michael <abdias.m@gmail.com>
2017-02-15 00:58:55 +0530

    change1

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 //console.log('hello world');
 var express = require('Express');
-var routes = require('./routes');
+var routes = require('./routes/index');
 var path = require('path');
-
+var fs = require('fs');
 var app = express();
+
+app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
     res.json({ message: 'Hello! Welcome to Express' });
 });
 
-//app.get('/index', routes.index);
+app.get('/index', routes.index);
 
-app.get('/index', function (req, res) {
-    res.type('html');
-    res.sendFile("/views/index.html");
-});
+//app.get('/index', function (req, res) {
+//    //res.type('html');
+//    //res.sendFile("/views/index.html");
+//    //res.send("Express");
+//    //var html = fs.readFileSync('./views/index.html', 'utf8')
+//    //res.render('test', { html: html })
+//});
 
 app.listen(7001);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 ﻿
 
 exports.index = function (req, res) {
-    res.type = "text/html";
-    res.sendFile('D:\workspace\github\HelloWorld\views\index.html');
+    //res.type = "text/html";
+    //res.sendFile('./views/index.html');
+    res.send('Express JS');
 };
